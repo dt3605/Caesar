9004f0254a6d905b5e3efc1d80611cffe408b1e2
Davi <daviteod2019@icloud.com>
2025-08-12 17:11:38 -0300

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -31,4 +31,51 @@ int main(int argc, char *argv[])
     char plaintext[1024];  // buffer para entrada
 
     printf("plaintext:  ");
-    if (fgets(plaintext, sizeof(plai
+    if (fgets(plaintext, sizeof(plaintext), stdin) == NULL)
+    {
+        printf("Error reading input.\n");
+        return 1;
+    }
+    // remove \n final se existir
+    plaintext[strcspn(plaintext, "\n")] = '\0';
+
+    // criptografa
+    printf("ciphertext: ");
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        printf("%c", rotate(plaintext[i], key));
+    }
+    printf("\n");
+
+    return 0;
+}
+
+// retorna true se todos dígitos
+bool only_digits(char *s)
+{
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (!isdigit((unsigned char) s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// rotaciona caractere
+char rotate(char c, int k)
+{
+    if (isupper((unsigned char) c))
+    {
+        return 'A' + (c - 'A' + k) % 26;
+    }
+    else if (islower((unsigned char) c))
+    {
+        return 'a' + (c - 'a' + k) % 26;
+    }
+    else
+    {
+        return c;
+    }
+}
